543ed58bd841b2dc0336f3714d3cdd7ee30e20aa
davelush <dave.lush@gmail.com>
2021-11-29 19:59:48 +0000

    level finish and crash now set back to beginning of level

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("you finished");
+        if (other.tag == "Player") {
+            SceneManager.LoadScene(0);
+        }
     }
 }
