cc2c39457d1a053601ab8d37335ff8f173f1a712
davelush <dave.lush@gmail.com>
2021-11-30 21:10:48 +0000

    added detectors and particle effects on finish line and crash. Also started adding a bunch of graphical tarting up

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    [SerializeField] float loadDelay = 1.0f;
+    [SerializeField] float loadDelay = 2.0f;
+    [SerializeField] ParticleSystem crashEffect;
+
+    // private void Start() {
+    //     Mesh mesh = GetComponent<MeshFilter>().mesh;
+    //     mesh.RecalculateBounds();
+    // }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground") {
+            crashEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
